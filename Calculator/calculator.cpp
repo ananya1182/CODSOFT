3a2e6f353efe9a91129fc127b42f2fc7a89cb5a8
Ananya <ananyasinghal0103@gmail.com>
2026-06-18 19:25:15 +0530

    Task-1 : Calculator added with input-output

---
+++ Calculator/calculator.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b;
+    char op;
+
+    cout << "Enter first number: ";
+    cin >> a;
+
+    cout << "Enter second number: ";
+    cin >> b;
+
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> op;
+
+    if(op == '+') cout << a + b;
+    else if(op == '-') cout << a - b;
+    else if(op == '*') cout << a * b;
+    else if(op == '/') {
+        if(b != 0) cout << a / b;
+        else cout << "Cannot divide by zero";
+    }
+    else cout << "Invalid operator";
+
+    return 0;
+}
